d01c11b7f3e3889a025d826361dbf98bafc19613
rodcaseribeiro <rodolfo2804@hotmail.com>
2021-04-14 11:34:34 -0300

    Create conf.js

---
+++ conf.js
@@ -0,0 +1,23 @@
+
+//npm install -g protractor
+//webdriver-manager update
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'firefox'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['vivo.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
